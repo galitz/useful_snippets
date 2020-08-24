01e020fb2e025be4d194c39e2c4240b73241991d
Geoff Galitz <ggalitz@shutterstock.com>
2020-08-24 12:44:19 +0000

    updates for cool docs

---
+++ various-cli-things.sh
@@ -0,0 +1,6 @@
+#
+# poor man's way to find how many threads are running for InsightVM AWS discovery connector polling
+grep AWS-Poller-Thread /opt/rapid7/nexpose/nsc/logs/eso.log | awk '{print $4}' | sort -u
+
+
+cat master_data  | jq '.orgs | length'  # JSON: count the number of orgs in master_data file
